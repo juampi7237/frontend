0313f65877f0c3b883144e0db465750e7c116750
Juan Pablo Gonzales <juan.gonzales.a@ucb.edu.bo>
2025-03-25 11:50:30 -0400

    fixaeado ruta por defecto cuando hace logout

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,12 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 export const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
-}; 
+};
